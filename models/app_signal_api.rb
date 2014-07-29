844454e97c03b298c6047f6056ed3541591054fd
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2014-07-29 17:20:06 +0100

    Working out aggregation of dates - WIP

--- models/app_signal_api.rb
+++ models/app_signal_api.rb
@@ -16,13 +16,12 @@ class AppSignalApi
 
 
 
-  def samples
+  def errors
     url = make_url('samples/errors')
     file = open url
     json_contents = file.read
     hash = JSON.parse(json_contents)
-    # File.open('jprint.json', 'w') { |fp| fp.puts(contents) }
-    # contents
+    result = analyse_hash(hash)
   end
 
 
@@ -31,6 +30,38 @@ class AppSignalApi
 
   def make_url(action)
     "#{@url}/#{@site_id}/#{action}.json?token=#{@token}"
+  end
+
+  def analyse_hash(hash)
+    num_errors_per_day = {}
+    total_errors = 0
+    6.downto(0) { |i| num_errors_per_day[(Date.today - i).to_time] = 0 }
+    dates = num_errors_per_day.keys
+
+    hash['log_entries'].each do | log |
+      log_date = determine_date[dates, log['time']]
+      unless log_date.nil?
+        total_errors += 1
+        num_errors_per_day[log_date] += 1
+      end
+    end
+  end
+
+
+  # determines which date the secs since epoch belongs to.  returns nil if before the first date
+  def determine_date(date_array, secs_since_epoch)
+    t = Time.at(secs_since_epoch)
+    return nil if t < date_array.first
+
+    # walk up the array until you get to ta date greater than the time, and then return that date.
+
+  end
+
+
+
+
+  def start_of_day(date)
+    date.to_time
   end
 
 
--- spec/models/app_signal_api_spec.rb
+++ spec/models/app_signal_api_spec.rb
@@ -14,5 +14,73 @@ describe AppSignalApi do
 
 
 
+  describe '#analyse_hash' do
+
+    let(:today)   { Date.today }
+    let(:api)     { AppSignalApi.new }
+
+    context 'more than 7 days of errors' do
+      it 'should return a hash summarising the number day by day' do
+        error_times = [
+            make_date_time(0, 3, 56, 45),
+            make_date_time(0, 5, 45, 3),
+            make_date_time(1, 15, 45, 3),
+            make_date_time(1, 16, 45, 3),
+            make_date_time(2, 5, 45, 3),
+            make_date_time(3, 5, 45, 3),
+            make_date_time(4, 5, 45, 3),
+            make_date_time(5, 5, 45, 3),
+            make_date_time(6, 5, 45, 3),
+            make_date_time(7, 5, 45, 3),
+            make_date_time(8, 5, 45, 3)
+        ]
+        hash = make_hash_from_error_times(error_times)
+        expected_result = construct_result(7, 9, [1,1,1,1,1,2,2])
+        expect(api.send(:analyse_hash)).to eq expected_result
+      end
+    end
+  end
+end
+
+
+def construct_result(num_days, num_errors, errors_per_day)
+  hash = {
+    'item' => [
+        { 'text' => "Past #{num_days} days", "value" => num_errors.to_s},
+        errors_per_day.map(&:to_s)
+    ]
+  }
+  hash.to_json
+
+end
+
+
+def make_hash_from_error_times(error_times)
+  log_entries = []
+  error_times.each do |et|
+    hash = {
+      'id'  => '53d42f00776f7272a7b20500',
+      'action' => 'ClaimController#download',
+      'path' => '/accelerated-possession-eviction/download',
+      'duration' => 1.584233,
+      'status' => nil,
+      'time' => et.to_i,
+      'is_exception' => true,
+      'exception' => {'name' => 'NoMethodError'}
+    }
+    log_entries << hash
+  end
+  { 'count' => 52, 'log_entries' => log_entries}
+
+end
+
+
+
+
+def make_date_time(days_ago, h, m, s)
+  date = Date.today - days_ago
+  time = date.to_time
+  time += ((h * 60 * 60) + (m * 60) + s)
+  time
 end
 
